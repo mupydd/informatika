a0deaa1086ab69eba4b87ed7b39a9b9e759dbc07
mupida <145311191+mupydd@users.noreply.github.com>
2024-09-01 11:50:28 +0700

    Add files via upload

---
+++ #penulisan statement.py
@@ -0,0 +1,19 @@
+#penulisan statement
+print("Hello World")
+
+#penulisan string
+Judul = "Liburan Ke Dufan"
+print(Judul)
+
+#penulisan case
+methode_name = "Muhfida Afifah Shofa"
+print(methode_name)
+
+#penulisan blok program
+a = 25
+b = 45
+if b > a:
+    print("b is greater than a")
+
+#penulisan komentar
+    """Menggunakan Triple Tanda Petik"""
